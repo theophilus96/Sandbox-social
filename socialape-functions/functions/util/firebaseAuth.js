b5521fb5be04aa8c41cc91b49cc2a35e0bd22184
theo <theophilus96@gmail.com>
2021-01-02 15:37:35 +0800

    util folder

--- socialape-functions/functions/util/firebaseAuth.js
+++ socialape-functions/functions/util/firebaseAuth.js
@@ -6,5 +6,29 @@ module.exports = (req, res, next) => {
     req.headers.authorization.startsWith("Bearer ")
   ) {
     idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "unathorized" });
   }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      req.user.imageUrl = data.docs[0].data().imageUrl;
+      return next();
+    })
+    .catch((err) => {
+      console.error("error while verifying token ", err);
+      return res.status(403).json(err);
+    });
 };
