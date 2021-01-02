7648401fab06f2357c1d03499d4cf6e4c39d0689
theo <theophilus96@gmail.com>
2021-01-02 14:16:34 +0800

    update

---
+++ socialape-functions/functions/util/firebaseAuth.js
@@ -0,0 +1,10 @@
+const { admin, db } = require("/admin");
+module.exports = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  }
+};
